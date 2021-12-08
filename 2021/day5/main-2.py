6233a6a4c938e8d23bedf0707b155a444bba12c2
Patrick Cockwell <pcockwell@gmail.com>
2021-12-08 01:14:28 -0800

    Advent of Code 2021 - Day 5

---
+++ 2021/day5/main-2.py
@@ -0,0 +1,66 @@
+def line_coordinates(line):
+  start, end = line.split(' -> ')
+  start_x, start_y = start.split(',')
+  start_x, start_y = int(start_x), int(start_y)
+  end_x, end_y = end.split(',')
+  end_x, end_y = int(end_x), int(end_y)
+
+  if start_x == end_x:
+    for y_val in range(min(start_y, end_y), max(start_y, end_y)+1):
+      yield (start_x, y_val)
+
+  elif start_y == end_y:
+    for x_val in range(min(start_x, end_x), max(start_x, end_x)+1):
+      yield (x_val, start_y)
+
+  else:
+    num_points = abs(end_x - start_x) + 1
+
+    subtract_offset_for_x = False
+    if end_x < start_x:
+      subtract_offset_for_x = True
+
+    subtract_offset_for_y = False
+    if end_y < start_y:
+      subtract_offset_for_y = True
+
+    for idx in range(num_points):
+      if subtract_offset_for_x:
+        x_val = start_x - idx
+      else:
+        x_val = start_x + idx
+
+      if subtract_offset_for_y:
+        y_val = start_y - idx
+      else:
+        y_val = start_y + idx
+
+      yield(x_val, y_val)
+
+
+num_dangerous_squares = 0
+vent_map = {}
+max_x = None
+max_y = None
+with open("data-1.txt", "r") as data_file:
+  for line in data_file.readlines():
+    for xcoord, ycoord in line_coordinates(line):
+      if not max_x or xcoord > max_x:
+        max_x = xcoord
+      if not max_y or xcoord > max_y:
+        max_y = ycoord
+
+      if ycoord not in vent_map:
+        vent_map[ycoord] = {}
+
+      if xcoord not in vent_map[ycoord]:
+        vent_map[ycoord][xcoord] = 0
+
+      vent_map[ycoord][xcoord] += 1
+
+  for row in vent_map.values():
+    for count in row.values():
+      if count > 1:
+        num_dangerous_squares += 1
+
+print(num_dangerous_squares)
